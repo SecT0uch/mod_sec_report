5bc12403c5a95f9a1559cd2026b6a61c1893c4d4
Jordan ERNST <pro.ernst@gmail.com>
2024-04-11 16:03:19 +0200

    Attempt to fix circular import

--- src/mod_sec_report_leoberry/__init__.py
+++ src/mod_sec_report_leoberry/__init__.py
@@ -1,6 +1,6 @@
 import argparse
 
-from mod_sec_report_leoberry import output, parse_modsec_file
+import mod_sec_report_leoberry
 
 parser = argparse.ArgumentParser(description="ModSecurity Report generator")
 parser.add_argument("logfiles",
@@ -13,6 +13,6 @@ args = parser.parse_args()
 
 entries = []
 for logfile in args.logfiles:
-    entries.extend(parse_modsec_file(logfile))
+    entries.extend(mod_sec_report_leoberry.parse_modsec_file(logfile))
 
-output(sorted(entries, key=lambda entry: entry["transaction"]["time_parsed"]))
+mod_sec_report_leoberry.output(sorted(entries, key=lambda entry: entry["transaction"]["time_parsed"]))
